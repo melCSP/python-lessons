d942968eeb5518f836114d7ac6ccd71023e3ab1f
melCSP <mel456689000@gmail.com>
2024-06-07 17:32:08 -0300

    Add files via upload

---
+++ day2 lesson.py
@@ -0,0 +1,21 @@
+#print (type (8 // 3))
+
+score = 0
+
+# user scores a point
+
+score += 1
+
+print (score)
+
+score = 0
+
+height = 1.8
+
+winning = True
+
+print ("your score is" + str(score))
+
+#f-string
+
+print("your score is {score}, your height is {height}, you are winning is {winning}")
---
+++ height = input ().py
@@ -0,0 +1,5 @@
+height = input ()
+weight = input ()
+weight_as_int = int (weight)
+height_as_float= float(height)
+bml = weight_as_int / height_as_float  ** 2
