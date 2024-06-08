fde26bbc41282ca4b28d40e1c9951a3866d1ec29
Melissa Silva <mel456689000@gmail.com>
2024-06-08 16:20:44 -0300

    Add files via upload

--- python 3 exercicio.py
+++ python 3 exercicio.py
@@ -7,9 +7,17 @@ if height >=120:
     if age< 12:
         print("please pay $5.")
     elif age <=18:
+        bill = 7
         print("please pay $7.")
+    elif age >= 45 and age <= 55:
+        print("everthing is going to be ok. have a ride on us")
     else:
         print("Please pay $12.")
+        bill = 12
+        wants_photo = input ("do you want a photo taken? y or N")
+        if wants_photo == "y":
+            bill = bill + 3
+        
 else:
     print("sorry you have to grow taller before you can ride")
     
